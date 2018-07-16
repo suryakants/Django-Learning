59cecb82c5d7f3e86ea901bd0a555384a6653c01
Suryakant Sharma <suryakant.sharma@imgainea.com>
2018-07-16 08:41:07 +0530

    m

--- Django Learning/mysite/personal/views.py
+++ Django Learning/mysite/personal/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def  index(request):
 	return render(request, 'personal/home.html')
+
+def contact(request):
+	return render(request, 'personal/basic.html',{"content" : ["If you have any query about our product, please email us","abc@gmail.com"]})
